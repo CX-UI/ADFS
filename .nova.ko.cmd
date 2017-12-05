2ae4f9857ff89d63da7df029013c1d31e73cefc6
CX-UI <tianfangmmr@126.com>
2017-12-05 11:56:25 +0800

    debug warning in dafs_dir.c

--- .nova.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/cx-dir/git/ADFS/nova.ko := ld -r -m elf_x86_64 -T ./scripts/module-common.lds --build-id  -o /home/cx-dir/git/ADFS/nova.ko /home/cx-dir/git/ADFS/nova.o /home/cx-dir/git/ADFS/nova.mod.o
